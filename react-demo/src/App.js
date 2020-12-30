11e1152eb6eec080571b7b9b6316050ad377855d
Armine Avdalyan <armineavdalyan85@gmail.com>
2020-12-30 16:13:33 +0400

    components

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Button from "./components/Button/Button.js";
+import Input from "./components/Input/Input.js";
+import Title from "./components/Title/Title.js";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <p>changed paragraph</p>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React2
-        </a>
-      </header>
+      <Title/>
+      <Input/>
+      <Button/>
     </div>
   );
 }
